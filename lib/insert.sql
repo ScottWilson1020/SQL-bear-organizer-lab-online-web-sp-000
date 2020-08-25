a5dc32513edfc22c5646e447e1a32a7bdde98e9b
Scott Wilson <scottwilson1020@gmail.com>
2020-08-25 17:53:05 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO bears (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+INSERT INTO bears (name, age, gender,) VALUES ('Maru', 3, 'Scottish Fold');
 
 
 INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
