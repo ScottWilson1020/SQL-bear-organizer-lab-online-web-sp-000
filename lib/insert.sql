ccb3b9e6503dd784e7e6109acd4d671cf2d74c52
Scott Wilson <scottwilson1020@gmail.com>
2020-08-25 17:57:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,3 @@
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Mr. Chocolate', 3, 'male',"brown", "Nice", TRUE);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Rowdy', 3, 'male',"brown", "Nice", TRUE);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Tabitha', 3, 'male',"brown", "Nice", TRUE);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Sergeant Brown', 3, 'male',"brown", "Nice", TRUE);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Melissa', 3, 'male',"brown", "Nice", TRUE);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Grinch', 3, 'male',"brown", "Nice", TRUE);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Wendy', 3, 'male',"brown", "Nice", TRUE);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('NULL', 3, 'male',"brown", "Nice", TRUE);
 
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 15, 'M', 'brown', 'calm', 1);
