608caf3aa31a829ea19ff37e893da68057021dda
Scott Wilson <scottwilson1020@gmail.com>
2020-08-25 17:56:20 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Mr. Chocolate', 3, 'male',"brown", "Nice", TRUE);
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Rowdy', 3, 'male',"brown", "Nice", TRUE);
 INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Maru', 3, 'male',"brown", "Nice", TRUE);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Maru', 3, 'male',"brown", "Nice", TRUE);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Maru', 3, 'male',"brown", "Nice", TRUE);
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('', 3, 'male',"brown", "Nice", TRUE);
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('', 3, 'male',"brown", "Nice", TRUE);
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('', 3, 'male',"brown", "Nice", TRUE);
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('', 3, 'male',"brown", "Nice", TRUE);
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('', 3, 'male',"brown", "Nice", TRUE);
 
